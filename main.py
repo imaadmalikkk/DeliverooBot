c4d45194d36d65a0c5da0ca84747d8ed4c0173ff
imaadmalikkk <68503037+imaadmalikkk@users.noreply.github.com>
2020-07-25 13:11:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,10 @@ from secret import CN
 from secret import Expiry
 from secret import CVV
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.chrome.options import Options
 
 
-PATH = "/Users/imaadmalik/Desktop/Development/DeliverooBot/chromedriver"
+PATH = "PATH OF THE CHROME DRIVER"
 driver = webdriver.Chrome(PATH)
-
 
 driver.get("https://deliveroo.co.uk")
 time.sleep(2) # time to allow the site to load to avoid an exception error
@@ -28,7 +26,7 @@ ContinueByEmail.click()
 time.sleep(2)
 # Enters my email
 Email = driver.find_element_by_class_name("ccl-f67359d1c16a266a")
-Email.send_keys("imaad@wearenwn.com")
+Email.send_keys("example@email.com")
 time.sleep(2)
 # Presses continue
 Continue = driver.find_element_by_class_name("ccl-cce251427bbe4ec4")
@@ -36,7 +34,7 @@ Continue.click()
 time.sleep(2)
 # Puts my password in from another file 
 Password = driver.find_element_by_id("login-password")
-Password.send_keys(pwd)
+Password.send_keys(pwd) # your password can either be entered as a string or imported from another file, which is what i did
 time.sleep(2)
 # Presses continue
 Go = driver.find_element_by_class_name("ccl-cce251427bbe4ec4")
@@ -44,7 +42,7 @@ Go.click()
 time.sleep(2)
 # Searches for my postcode
 searchLocation = driver.find_element_by_id("location-search")
-searchLocation.send_keys("WD19 4JH")
+searchLocation.send_keys("POSTCODE")
 searchLocation.send_keys(Keys.RETURN)
 time.sleep(2)
 Food = driver.find_element_by_class_name("HomeSearch-b8d615a5b6ee28c6")
@@ -70,10 +68,12 @@ Checkout = driver.find_element_by_xpath("/html/body/div[3]/div/div[2]/div[3]/div
 Checkout.click()
 time.sleep(2)
 Street = driver.find_element_by_id("address1")
-Street.send_keys("58 Eastbury Road")
+Street.send_keys("HOUSE NUMBER AND ROAD")
 Note = driver.find_element_by_id("delivery_note")
-Note.send_keys("Message when you are here and I will come down and collect")
+Note.send_keys("RIDER DELIVERY NOTES")
+'''my card is automatically saved to my account so all the bot has to do is press submit order. If your card isnt saved you will have to
+add the xpaths of each field and tell selenium to input your details'''
 #Submit = driver.find_element_by_xpath("//*[@id='payment-app-container']/div[2]/div/div/div[1]/form/div[6]/div/span/button/span")
 #Submit.click()
-time.sleep(3600)
+time.sleep(3600) # waits 1 hour before it closes, so you can track the order status
 
